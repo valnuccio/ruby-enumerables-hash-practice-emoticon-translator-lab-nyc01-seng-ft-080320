519ce75d7cf87f238ebb5c6405f23d9c49749ca7
Valerie Nuccio <val.nuccio@gmail.com>
2020-07-31 17:19:44 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -15,8 +15,8 @@ end
 def get_english_meaning(file_path, emoticon)
   library=load_library(file_path)
   library.each do |english_name,e_and_j_emoji|
-  if e_and_j_emoji[:japanese]==emoticon
-    return english_name
+    if e_and_j_emoji[:japanese]==emoticon
+      return english_name
     end
   end
 end
