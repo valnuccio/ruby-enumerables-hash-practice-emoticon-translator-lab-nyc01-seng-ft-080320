dd1131fc4c4f7bca9d602344f45187ea08012534
Valerie Nuccio <val.nuccio@gmail.com>
2020-07-31 17:15:58 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -14,7 +14,8 @@ end
 
 def get_english_meaning(file_path, emoticon)
 library=load_library(file_path)
-library.each do |key,value|
+library.each do |english_name,inner_hash
+  
 binding.pry
   end
 end
