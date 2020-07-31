f1269829d1632ee30109d85b9af17266d8749135
Valerie Nuccio <val.nuccio@gmail.com>
2020-07-31 17:51:31 +0000

    Automatically backed up by Learn

--- lib/translator.rb
+++ lib/translator.rb
@@ -31,6 +31,7 @@ def get_japanese_emoticon(file_path, emoticon)
       return e_and_j_emoji[:japanese]
     end
   end
+  "Sorry, that emoticon was not found"
 end
 
 # def get_japanese_meaning (file_path, emoticon)
